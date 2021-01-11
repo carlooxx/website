3e95170e02da5b3152293bde8b29a3c139e7fd35
carlooxx <karlo.braovac@gmail.com>
2021-01-11 17:53:39 +0100

    5. Route

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import React from "react";
 import AboutUs from "../src/pages/AboutUs"
 import GlobalStyle from '../src/components/GlobalStyle'
 import Nav from '../src/components/Nav';
+import OurWork from '../src/pages/OurWork'
+import ContactUs from '../src/pages/ContactUs'
+import {Switch, Route} from 'react-router-dom'
 
 function App() {
   return(
     <div className="app">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path='/' exact>
+          <AboutUs />
+        </Route>
+        <Route path='/work'>
+          <OurWork />
+        </Route>
+        <Route path='/contact'>
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   )
 }
