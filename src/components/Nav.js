38fbb11561540d7ecdb925b1399e7edc36ec9694
carlooxx <karlo.braovac@gmail.com>
2021-01-13 13:37:09 +0100

    9. Nav Line

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import { useLocation } from "react-router-dom";
 const Nav = () => {
+  const { pathname } = useLocation();
   return (
     <StyledNav>
       <h1>
@@ -12,12 +15,27 @@ const Nav = () => {
       <ul>
         <li>
           <Link to="/">1.About Us</Link>
+          <StyledLine
+            transition={{ duration: 0.75 }}
+            initial={{ width: "0%" }}
+            animate={{ width: pathname === "/" ? "40%" : "0%" }}
+          />
         </li>
         <li>
           <Link to="/work">2.Our Work</Link>
+          <StyledLine
+            transition={{ duration: 0.75 }}
+            initial={{ width: "0%" }}
+            animate={{ width: pathname === "/work" ? "40%" : "0%" }}
+          />
         </li>
         <li>
           <Link to="/contact">3.Contact Us</Link>
+          <StyledLine
+            transition={{ duration: 0.75 }}
+            initial={{ width: "0%" }}
+            animate={{ width: pathname === "/contact" ? "40%" : "0%" }}
+          />
         </li>
       </ul>
     </StyledNav>
@@ -53,5 +71,13 @@ const StyledNav = styled.nav`
     padding-left: 10rem;
     position: relative;
   }
+`;
+
+const StyledLine = styled(motion.div)`
+  height: 0.3rem;
+  width: 0%;
+  background: #23d997;
+  position: absolute;
+  margin-top: 0.5rem;
 `;
 export default Nav;
